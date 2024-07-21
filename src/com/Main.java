8c08d17fa482389e0aae93936be265ec390d10aa
Usevalad <avm012@gmail.com>
2024-07-21 13:04:14 +0100

    3

--- src/com/Main.java
+++
@@ -1,9 +0,0 @@
-import com.newPackage.LessonThree;
-
-public class Main {
-    public static void main(String[] args) {
-        LessonThree summa = new LessonThree();
-
-        System.out.println(summa.array());
-    }
-}
