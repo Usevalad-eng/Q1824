a2bcf2f0a350640ad18ef15fd46fec4b161b6238
Usevalad <avm012@gmail.com>
2024-07-21 12:29:43 +0100

    21july24_2

---
+++ src/com/H1/task3/ExitProgramm.java
@@ -0,0 +1,28 @@
+package com.H1.task3;
+
+import java.util.Scanner;
+
+public class ExitProgramm {
+    public static void main(String[] args) {
+        String a = "exit";
+        //String b = " ";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter one word");
+        String k = sc.nextLine();
+
+
+        if (k.equals(a)) {
+            System.out.println("Can exit");
+
+        } else {
+            System.out.println("You entered something");
+            System.out.println("Enter another word");
+
+            String c = sc.nextLine();
+            System.out.println("You entered " + c);
+            System.out.println("Can exit");
+
+        }
+
+    }
+}
---
+++ src/com/H1/task4/DivideByThreeApp.java
@@ -0,0 +1,28 @@
+package com.H1.task4;
+import java.util.Scanner;
+
+public class DivideByThreeApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number");
+        int num2 = sc.nextInt();
+
+        if ((num1 % num2 == 0) || num1 <= num2) {
+
+            int i = 0;
+
+            int sum;
+            for (sum = num1; sum <= num2; i++) {
+                sum += i;
+
+            }
+
+            System.out.println(sum);
+        } else {
+            System.out.println("Error, can't devide");
+        }
+
+    }
+}
