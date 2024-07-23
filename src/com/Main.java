721a77529dc774224544a3c135014860944886c0
Usevalad <avm012@gmail.com>
2024-07-23 12:30:16 +0100

    commit 23 july 2024

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,3 +1,5 @@
+import homeTasks.homeTask2.DifferenceOfElements;
+
 public class Main {
     public static void main(String[] args) {
 //        LessonThree summa = new LessonThree();
@@ -10,15 +12,19 @@ public class Main {
         //Cars car = new Cars();
         //car.cars();
 //-------------------------------------------------------
+        //homeTask1
         //VowelOrNot letter = new VowelOrNot();
         //letter.vowelOrNot();
 //------------------------------------------------------
-         //Season ses = new Season();
+        //homeTask1
+        //Season ses = new Season();
          //ses.season();
 //------------------------------------------------------
+        //homeTask1
         //ExitProgramm exit = new ExitProgramm();
         //exit.input();
 //-----------------------------------------------------
+        //homeTask1
         //DivideByThreeApp div = new DivideByThreeApp();
         //div.divByThree();
 //-----------------------------------------------------
@@ -26,5 +32,15 @@ public class Main {
  //       FibonachiArray arr = new FibonachiArray();
  //       arr.arrayFibonachi();
  //----------------------------------------------------
+        //homeTask2.2
+ //       MinMaxElement minMaxElem=new MinMaxElement();
+ //       minMaxElem.minMaxElement();
+ //----------------------------------------------------
+        //homeTask2.3
+        DifferenceOfElements dif = new DifferenceOfElements();
+        dif.difference();
+
+
+
     }
 }
